d58ec73fe26d338b2dc51d05cef5be5e925ac23b
AkiroKazuki <agungwah900@gmail.com>
2024-09-28 08:44:42 +0800

    update spinner, theme switch, faster spinner

--- app/public/js/spinner.js
+++ app/public/js/spinner.js
@@ -9,7 +9,7 @@
                  spinner.classList.add('hide'); // Tambahkan class 'hide' untuk menghilangkan spinner
                  console.log("Spinner hidden");
              }
-         }, 1000); // Delay 1 detik sebelum menyembunyikan spinner
+         }, 400); // Delay 400ms sebelum menyembunyikan spinner
      };
  
      // Panggil hideSpinner ketika semua konten selesai loading
