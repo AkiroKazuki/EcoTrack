1ab7488e217ecc4b6ed55aa1e9e2499f7147ca62
DePram <dprameshwara@gmail.com>
2024-10-04 14:30:47 +0800

    tambah switch di main navbar

--- app/public/js/darkmode.js
+++ app/public/js/darkmode.js
@@ -1,31 +1,36 @@
-const checkbox = document.getElementById('checkbox');
+const checkbox = document.getElementById("checkbox");
+const secondCheckbox = document.getElementById("checkboxSecond");
 
 // update switch
 const setTheme = (theme) => {
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme);
-    checkbox.checked = (theme === 'dark'); // update switch berdasarkan tema yang dpilih
+  document.documentElement.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
+  checkbox.checked = theme === "dark"; // update switch berdasarkan tema yang dpilih
+  secondCheckbox.checked = theme === "dark"; // update switch berdasarkan tema yang dpilih
 };
 
 // local storage check
-const savedTheme = localStorage.getItem('theme');
+const savedTheme = localStorage.getItem("theme");
 
 // tema yang diganti user
-const currentTheme = savedTheme || (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+const currentTheme = savedTheme || (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
 
 // tema default user
 setTheme(currentTheme);
 
 // ganti tema
-checkbox.addEventListener('change', () => {
-    const theme = checkbox.checked ? 'dark' : 'light';
+checkbox.addEventListener("change", () => {
+  const theme = checkbox.checked ? "dark" : "light";
+  setTheme(theme);
+});
+secondCheckbox.addEventListener("change", () => {
+  const theme = secondCheckbox.checked ? "dark" : "light";
+  setTheme(theme);
+});
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
+  if (!savedTheme) {
+    const theme = event.matches ? "dark" : "light";
     setTheme(theme);
+  }
 });
-
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-    if (!savedTheme) {
-        const theme = event.matches ? 'dark' : 'light';
-        setTheme(theme);
-    }
-});
-
